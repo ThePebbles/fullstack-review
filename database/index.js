6e80bf9eebac56c502099d74528bd24e441f6a86
Sarah <sarahkwando@gmail.com>
2023-01-31 19:48:11 -0600

    Complete save function for database

--- database/index.js
+++ database/index.js
@@ -20,10 +20,85 @@ let repoSchema = mongoose.Schema({
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (...repos) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  return repos.forEach((repo) => {
+    Repo.findOne({'repoId': repo.id})
+    .then((results) => {
+      if (!results) {
+        return Repo.create({
+          'repoId': repo.id,
+          'repoName': repo.name,
+          'repoUrl': repo.url,
+          'description': repo.description,
+          'ownerId': repo.owner.id,
+          'ownerLogin': repo.owner.login,
+          'forksCount': repo.fork_count
+        })
+        .then((repoEntry) => {
+          return repoEntry.save()
+        })
+        .catch((err) => {
+          console.log('Error in save: ', err);
+        })
+      } else {
+        return Repo.update({
+          'repoId': repo.id,
+          'repoName': repo.name,
+          'repoUrl': repo.url,
+          'description': repo.description,
+          'ownerId': repo.owner.id,
+          'ownerLogin': repo.owner.login,
+          'forksCount': repo.fork_count
+        })
+        .then((repoEntry) => {
+          return repoEntry.save()
+        })
+        .catch((err) => {
+          console.log('Error in save: ', err);
+        })
+      }
+    });
+  });
+  // basic logic
+  //   var repoEntry = Repo.create({   or Repo.update
+  //     'repoId': repo.id,
+  //     'repoName': repo.name,
+  //     'repoUrl': repo.url,
+  //     'description': repo.description,
+  //     'ownerId': repo.owner.id,
+  //     'ownerLogin': repo.owner.login,
+  //     'forksCount': repo.fork_count
+  //   });
+  //   repoEntry.save()
+  //   .then(() => {
+  //     console.log('Repo entry saved!')
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  // });
+  // Another option for iterating over all repos
+  // for (var i = 0; i < repos.length; i++) {
+  //   var repoEntry = new Repo({
+  //     'repoId': repos[i].id,
+  //     'repoName': repos[i].name,
+  //     'repoUrl': repos[i].url,
+  //     'description': repos[i].description,
+  //     'ownerId': repos[i].owner.id,
+  //     'ownerLogin': repos[i].owner.login,
+  //     'forksCount': repos[i].fork_count
+  //   });
+  //   repoEntry.save()
+  //   .then(() => {
+  //     console.log('Repo entry saved!')
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  // }
 }
 
 module.exports.save = save;
